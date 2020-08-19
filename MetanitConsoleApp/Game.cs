1d5fb3174281a2f8eab0a7966ba9eb632af8b451
SilentHill <SilentHill.748@gmail.com>
2020-08-19 17:53:49 +0500

    Update Game.cs

--- MetanitConsoleApp/Game.cs
+++ MetanitConsoleApp/Game.cs
@@ -1,8 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MetanitConsoleApp
 {
@@ -20,7 +16,6 @@ namespace MetanitConsoleApp
 
             while (true)
             {
-
                 UserKey = SetUserKey();
 
                 Logic(SecretNum, UserKey, out int cows, out int bulls);
